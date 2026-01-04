ff9ff84fea653b5c6280fb4f5087fb5b7533f251
Naman9plus0 <9431418824az@gmail.com>
2026-01-04 19:57:06 +0530

    Add files via upload
    1.Write a Java program to print your name, age, and city.
    
    2.Print a square pattern using *.
    
    3.Declare variables for student name, roll number, and marks.
    
    4.Store two numbers and print their sum, difference, product, and division.
    
    5.Convert temperature from Celsius to Fahrenheit.

---
+++ Day1.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+// ans 1
+// public class day {
+
+// public static void main(String[] args) {
+// System.out.println(" my name is NAMAN ");
+// System.out.println("My age is 19");
+// System.out.println(" i live in sasaram ");
+// }
+
+// }
+
+// ===============================================================================
+
+// ans 2
+// public class day {
+// public static void main(String[] args) {
+// System.out.println("*****");
+// System.out.println("*****");
+// System.out.println("*****");
+// System.out.println("*****");
+// System.out.println("*****");
+
+// }
+// }
+
+// =====================================================================================
+
+// ans 3
+// public class day {
+// public static void main(String[] args) {
+// String student_name = "naman";
+// int roll = 10;
+// double marks = 67.9;
+// System.out.println("Student name = " + student_name);
+// System.out.println("ROLL =" + roll);
+// System.out.println("MARKS " + marks);
+
+// }
+// }
+
+// ===========================================================
+
+// ans4
+
+// public class day {
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// System.out.println("enter first no");
+// int a = sc.nextInt();
+// System.out.println("enter second no");
+// int b = sc.nextInt();
+// int sum = a + b;
+// int sub = a - b;
+// int product = a * b;
+// int div = a / b;
+// System.out.println("SUM" + sum);
+// System.out.println("difference" + sub);
+// System.out.println("product" + product);
+// System.out.println("division" + div);
+// }
+// }
+
+// =========================================================================
+
+// ans5
+
+// public class day {
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// System.out.println("enter temp in celsius");
+// double celsius = sc.nextDouble();
+// double fah = ((celsius) * 9) / 5 + 32;
+// System.out.println("fahrenheit =" + fah);
+
+// }
+// }
