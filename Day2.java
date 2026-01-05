8f9312888fc118ee32ca922aa513311e7a79577b
Naman9plus0 <9431418824az@gmail.com>
2026-01-05 22:33:03 +0530

    Add files via upload
    6. Take user input and print it back.
    
    7.  Find the area of a circle using user input.
    
    8.  Swap two numbers without using a third variable.
    
    9.  Check whether a number is positive or negative.
    
    10.  Print your name 10 times using a loop.

---
+++ Day2.java
@@ -0,0 +1,97 @@
+
+// // // import java.util.Scanner;
+
+// // // ANS =6
+
+// // // import java.util.Scanner;
+
+// // // public class Day2 {
+// // // public static void main(String[] args) {
+
+// // // Scanner sc = new Scanner(System.in);
+// // // System.out.println("ENter a Number");
+// // // int a = sc.nextInt();
+// // // System.out.println("Your Number =" + a);
+// // // }
+
+// // // }
+
+// // //=====================================================================
+
+// // //ANS = 7
+
+// // // public class Day2 {
+// // // public static void main(String[] args) {
+// // //import java.util.Scanner;
+// // // Scanner sc = new Scanner(System.in);
+// // // System.out.println("Enter Radius");
+// // // double r = sc.nextDouble();
+
+// // // double area = (22 * r * r) / 7;
+// // // System.out.println("AREA =" + area);
+
+// // // }
+// // // }
+
+// // //==================================================================
+
+// // //ans 8
+// // // import java.util.Scanner;
+
+// // // public class Day2 {
+// // // public static void main(String[] args) {
+
+// // // Scanner sc = new Scanner(System.in);
+// // // System.out.println("Enter first no");
+// // // int a = sc.nextInt();
+
+// // // System.out.println("enter 2nd no");
+// // // int b = sc.nextInt();
+// // // a = a - b;// a=10 , b=4 . a = 10-4=6 (a= 6, b= 4 )
+// // // b = b + a; // b = 4+6= 10
+// // // a = b - a;
+// // // System.out.println(" First no after change " + a);
+// // // System.out.println(" Second no after change " + b);
+
+// // // }
+// // // }
+
+// //
+// //======================================================================================
+
+// // //ans 9
+// // import java.util.Scanner;
+
+// // public class Day2 {
+// // public static void main(String[] args) {
+
+// // Scanner sc = new Scanner(System.in);
+// // System.out.println("Enter a number");
+// // int Number = sc.nextInt();
+// // if (Number * 1 != 0) {
+
+// // if (Number * 1 == Number) {
+// // System.out.println("It is positive");
+// // } else {
+// // System.out.println("It is negative");
+// // }
+// // } else {
+// // System.out.println("number is zero");
+// // }
+
+// // }
+// // }
+
+// //==========================================================================================
+
+// // ans = 10
+
+// public class Day2 {
+// public static void main(String[] args) {
+// int p = 10;
+// for (int i = 1; i <= p; i++) {
+// System.out.println("my name is NAMAN ");
+// }
+
+// }
+// }
