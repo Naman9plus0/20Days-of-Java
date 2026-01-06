d2289d6ce00423bfb4ebd6cff180815081225cf0
Naman9plus0 <9431418824az@gmail.com>
2026-01-06 15:41:33 +0530

    Add files via upload
    11. Display ASCII value of a character.
    
    12. Convert minutes into hours and minutes.
    
    13. Calculate simple interest.
    
    14. Check if a number is divisible by 5.
    
    15. Print the first 10 natural numbers.

---
+++ Day3.java
@@ -0,0 +1,82 @@
+
+// import java.util.Scanner;
+
+// // import java.util.Scanner;
+
+// // // import java.util.Scanner;
+
+// // // // //ans = 11
+// // // // public class Day3 {
+// // // //     public static void main(String[] args) {
+// // // //         char character = 'A';
+// // // //         int asciivaluve = character;
+// // // //         System.out.println("The AsciiValue of character is " + asciivaluve);
+// // // //     }
+
+// // // // }
+
+// // // //===========================================================================================================
+
+// // // //Ans = 12 
+
+// // // public class Day3 {
+// // //     public static void main(String[] args) {
+// // //         Scanner sc = new Scanner(System.in);
+// // //         System.out.println("Enter minutes ");
+// // //         int minute = sc.nextInt();
+// // //         int a = minute;
+// // //         minute = minute % 60;
+// // //         int hour = a / 60;
+// // //         System.out.println("HOUR =  " + hour + " " + ":" + "MINUTES = " + minute);
+
+// // //     }
+// // // }
+
+// // //================================================================================================================
+
+// // //Ans = 13
+// // // calculate simple interst
+
+// // public class Day3 {
+// //     public static void main(String[] args) {
+// //         Scanner sc = new Scanner(System.in);
+// //         System.out.println("Enter Amount  ");
+// //         int Amount = sc.nextInt();
+// //         System.out.println("Enter Year  ");
+// //         int Year = sc.nextInt();
+// //         System.out.println("Enter Interst %  ");
+// //         int interst = sc.nextInt();
+// //         int SI = (Amount * Year * interst) / 100;
+// //         System.out.println("Your Simple Interest is  " + SI);
+
+// //     
+// // }
+
+// //===========================================================================================================
+
+// //Ans 14 
+
+// public class Day3 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter a Number");
+//         int Number = sc.nextInt();
+//         if (Number % 5 == 0) {
+//             System.out.println("Number is Divisble by 5");
+//         } else {
+//             System.out.println("not Divisible by  5 ");
+//         }
+//     }
+// }
+
+//============================================================================================
+
+//Ans 15
+
+public class Day3 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
