0a73e79964b96a03226f1a47883b3fd134ba0bc1
Naman9plus0 <9431418824az@gmail.com>
2026-01-07 19:04:39 +0530

    Add files via upload
    16. Check if a number is even or odd.
    
    17. Find the largest of three numbers.
    
    18. Check whether a year is a leap year.
    
    19. Create a grading system using marks.

---
+++ Day4.java
@@ -0,0 +1,108 @@
+
+// // // import java.util.Scanner;
+
+// // // //Ans 16
+
+// // // // import java.util.Scanner;
+
+// // // // public class Day4 {
+// // // // public static void main(String[] args) {
+// // // // Scanner sc = new Scanner(System.in);
+// // // // System.out.println("Enter a Number ");
+// // // // int num = sc.nextInt();
+// // // // if (num != 0) {
+// // // // if (num % 2 == 0) {
+// // // // System.out.println("number is EVEN");
+// // // // } else {
+// // // // System.out.println("odd");
+// // // // }
+// // // // } else {
+// // // // System.out.println("niether even nor odd");
+// // // // }
+
+// // // // }
+
+// // // // }
+
+// // //
+// //
+// //==========================================================================================================
+
+// // // //Ans17
+
+// // // public class Day4 {
+// // // public static void main(String[] args) {
+// // // Scanner sc = new Scanner(System.in);
+// // // System.out.println("Enter three number which you want to compare ");
+// // // int a = sc.nextInt();
+// // // int b = sc.nextInt();
+// // // int c = sc.nextInt();
+// // // if (a > c) {
+// // // if (b > a) {
+// // // System.out.println(" 2nd no. is largest");
+// // // } else {
+// // // System.out.println("1st no. is largest");
+// // // }
+// // // } else {
+// // // System.out.println("3rd one is largest ");
+// // // }
+
+// // // }
+// // // }
+
+// //
+// //==========================================================================================================
+
+// // //Ans 18
+
+// // import java.util.Scanner;
+
+// // public class Day4 {
+// // public static void main(String[] args) {
+// // Scanner sc = new Scanner(System.in);
+// // System.out.println("Enter a year ");
+// // int y = sc.nextInt();
+// // if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0)) {
+// // System.out.println(" leap year");
+// // } else {
+// // System.out.println(" not leap year ");
+// // }
+// // }
+
+// // }
+
+// //
+// ===================================================================================================================
+
+// // Ans 19
+
+// import java.util.Scanner;
+
+// public class Day4 {
+
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// System.out.println("enter marks");
+// int n = sc.nextInt();
+// if (n > 90) {
+// System.out.println("proper class ");
+// } else if (n > 80) {
+// System.out.println("wow");
+// } else if (n > 70) {
+// System.out.println("beech mein fas gya hu");
+// } else if (n > 60) {
+// System.out.println("1st division se pass bani");
+// } else if (n > 50) {
+// System.out.println("padhle bhai");
+// } else if (n > 40) {
+// System.out.println("chii sasur");
+// } else {
+// System.out.println("fail");
+// }
+
+// }
+// }
+
+// ===============================================================================================================
+
+// Ans 20
