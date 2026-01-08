68266e6b05a86f8562998c3fdbd5884ca18e72b0
Naman9plus0 <9431418824az@gmail.com>
2026-01-08 16:35:58 +0530

    Add files via upload
    20 .Create a menu-based calculator using switch.
    
    21. Print numbers from 1 to 100.
    
    22. Print even numbers between 1 and 50.
    
    23. Find the sum of numbers from 1 to 100.
    
    24. Reverse a number using a loop

---
+++ Day5.java
@@ -0,0 +1,116 @@
+
+// import java.util.Scanner;
+
+// //Ans 20
+// public class Day5 {
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// System.out.println("---MENU---");
+// System.out.println("1. Addition ");
+// System.out.println("2. Subtraction ");
+// System.out.println("3. Multiplication");
+// System.out.println("4. Division");
+// System.out.println("Enter choice (1-4) =");
+// int choice = sc.nextInt();
+// System.out.println("Enter num 1");
+// double num1 = sc.nextDouble();
+// System.out.println("Enter num2 ");
+// double num2 = sc.nextDouble();
+// switch (choice) {
+// case 1:
+// System.out.println("Result " + (num1 + num2));
+// break;
+// case 2:
+// System.out.println("Result" + (num1 - num2));
+
+// break;
+// case 3:
+// System.out.println("Result " + (num1 * num2));
+// break;
+// case 4:
+// if (num2 != 0) {
+// System.out.println("Result" + (num1 / num2));
+// } else {
+// System.out.println("not possible");
+// }
+
+// break;
+// default:
+// System.out.println("InvLID choice , plz choose btw (1-4)");
+// }
+// sc.close();
+
+// }
+// }
+
+// //
+// =====================================================================================================================
+
+// // import java.util.Scanner;
+
+// // // // //Ans 21
+
+// // // // // public class Day5 {
+// // // // // public static void main(String[] args) {
+
+// // // // // for (int i = 1; i <= 100; i++) {
+// // // // System.out.println(i);
+// // // // // }
+// // // // // }
+// // // // // }
+
+// // // //
+// //
+// //===================================================================================================================
+
+// // // // //Ans 22
+
+// // // // public class Day5 {
+// // // // public static void main(String[] args) {
+// // // // for (int i = 1; i <= 50; i++) {
+// // // // if (i % 2 == 0) {
+// // // // System.out.println(i);
+// // // // }
+// // // // }
+// // // // }
+// // // // }
+
+// // //
+// //
+// //========================================================================================================================
+
+// // // //Ans 23
+
+// // // public class Day5 {
+// // // public static void main(String[] args) {
+// // // int n = 100;
+// // // int sum = n / 2 * (1 + n);
+// // // System.out.println("SUM from 1 to 100 == " + sum);
+
+// // // }
+// // // }
+
+// //
+// //=============================================================================================================================
+
+// // //Ans 24
+
+// // public class Day5 {
+// // public static void main(String[] args) {
+// // Scanner sc = new Scanner(System.in);
+// // System.out.println("enter number ");
+// // int n = sc.nextInt();
+// // int a = 0;
+
+// // while (n > 0) {
+// // a = a * 10 + n % 10;
+// // n = n / 10;
+
+// // }
+// // System.out.println(" reverse number " + a);
+
+// // }
+// // }
+
+// //
+// =======================================================================================================================
